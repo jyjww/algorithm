655688a74932cdc3f858916ee36340a5cab43886
YeJee Jang <158022863+jyjww@users.noreply.github.com>
2025-12-23 12:30:46 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 596 ms, Memory: 66080 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,23 +1,31 @@
 import sys
-from collections import defaultdict, deque
 sys.setrecursionlimit(10**6)
-def dfs (v):
-    visited[v] = True
-    for v_next in graph[v]:
-        if not visited[v_next] :
-            dfs(v_next)
 input = sys.stdin.readline
-N, E = map(int, input().split())
-graph = defaultdict(list)
-visited = [False]*(N+1)
-count = 0
 
-for _ in range(E):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
-for i in range(1, N+1):
-    if not visited[i]:  
-        dfs(i)
-        count += 1     
-print(count)
+n, m = map(int, input().split())
+
+# 인접 리스트 만들기
+graph = [[] for _ in range (n+1)]
+
+# 간선 정보 입력
+for _ in range(m):
+  a, b = map(int, input().split())
+  graph[a].append(b)
+  graph[b].append(a)
+
+# 재귀로 dfs 인접 노드 탐색 : x에서 출발해서 갈 수 있는 모든 노드를 다 방문
+visited = [False] * (n+1)
+def dfs(x):
+  visited[x] = True
+  for nxt in graph[x]:
+    if not visited[nxt]:
+      dfs(nxt)
+
+# 노드 1부터 n까지 확인. 아직 방문 안 한 노드를 발견하면 그 노드에서 DFS 시작
+cnt = 0
+for i in range(1, n+1):
+  if not visited[i]:
+    dfs(i)
+    cnt += 1 
+
+print(cnt)  
